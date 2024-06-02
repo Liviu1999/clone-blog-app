503ebf823d66ba17e10d44c67438ff0195e6f1e2
Liviu1999 <101752562+Liviu1999@users.noreply.github.com>
2024-06-02 19:52:29 +0200

    bis2

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/clone-blog-app",
+  base: "/clone-blog-app/",
   plugins: [react()],
   server: {
     proxy: {
